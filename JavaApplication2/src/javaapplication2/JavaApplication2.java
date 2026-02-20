eb0f5437bacccfc8b0f431b111f430310cb3a5fa
ST10486281 <ST10486281@rcconnect.edu.za>
2026-02-20 15:33:42 +0200

    1

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -1,40 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package javaapplication2;
 
 import java.util.Scanner;
 
-/**
- *
- * @author lab_services_student
- */
 public class JavaApplication2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        // String myName = "Riko";
-        // char in = 'd';
-        // int age = 8;
-        // double exactAge = 8.95;
-        // System.out.print("Hello " + myName);
-        // System.out.println("/Please enter you id.");
-        // // return void;
 
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter your name: ");
         String name = input.nextLine();
 
-        System.out.println("\nHello \t" + name);
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        System.out.println("\nHello " + name + "! You are " + age + " years old!");
+
+        input.close();
     }
-
-    // int methodExample(){
-
-    // }
-
 }
