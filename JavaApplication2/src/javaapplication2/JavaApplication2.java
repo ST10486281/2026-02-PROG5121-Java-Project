d98b67d52a85778d6232511a1902726e331d1a56
ST10486281 <ST10486281@rcconnect.edu.za>
2026-02-20 16:10:35 +0200

    1

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -8,19 +8,21 @@ public class JavaApplication2 {
     private String name;
     private int age;
 
- 
+    private Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        JavaApplication2 app = new JavaApplication2();
-       
-        Scanner input = new Scanner(System.in);
+    public void UserInputCode() {
 
         System.out.print("Enter your name: ");
         name = input.nextLine();
 
         System.out.print("Enter your age: ");
         age = input.nextInt();
-       
+
         System.out.println("Hello " + name + "! You are " + age + " years old!");
+    }
+
+    public static void main(String[] args) {
+        JavaApplication2 app = new JavaApplication2();
+        app.UserInputCode();
     }
 }
