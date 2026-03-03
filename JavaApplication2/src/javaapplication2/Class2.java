cccfab6feef96fc313f802790b122fa3be634005
ST10486281 <ST10486281@rcconnect.edu.za>
2026-03-03 13:14:24 +0200

    1

---
+++ JavaApplication2/src/javaapplication2/Class2.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Class2 {
+
+    public static void main(String[] args) {
+        JOptionPane.showInputDialog(null, "fff");
+    }
+}
