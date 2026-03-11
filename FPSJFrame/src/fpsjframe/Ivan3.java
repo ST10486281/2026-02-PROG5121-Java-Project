2509caa634cd0da6901f13dde1b4b376a7746edf
ST10486281 <ST10486281@rcconnect.edu.za>
2026-03-11 14:12:18 +0200

    1

--- FPSJFrame/src/fpsjframe/Ivan3.java
+++ FPSJFrame/src/fpsjframe/Ivan3.java
@@ -238,7 +238,6 @@ public class FPSJFrame extends JPanel implements KeyListener, Runnable {
 			double fStepSize = 0.005;
 			double fDistanceToWall = 0;
 			boolean bHitWall = false;
-			// boolean bBoundary = false;
 
 			double fEyeX = Math.cos(fRayAngle);
 			double fEyeY = Math.sin(fRayAngle);
@@ -255,24 +254,20 @@ public class FPSJFrame extends JPanel implements KeyListener, Runnable {
 				} else if (map[nTestY].charAt(nTestX) == '#') {
 					bHitWall = true;
 
-					// // Check for boundary (edge between wall tiles)
-					// double[] fBoundVecX = new double[4];
-					// double[] fBoundVecY = new double[4];
-
+					// -- Wall edge outlines (commented out to hide lines) --
+					// boolean bBoundary = false;
 					// for (int tx = 0; tx < 2; tx++) {
 					// for (int ty = 0; ty < 2; ty++) {
-					// int idx = tx * 2 + ty;
 					// double vx = (double) nTestX + tx - fPlayerX;
 					// double vy = (double) nTestY + ty - fPlayerY;
 					// double d = Math.sqrt(vx * vx + vy * vy);
 					// double dot = (fEyeX * vx / d) + (fEyeY * vy / d);
-					// fBoundVecX[idx] = vx;
-					// fBoundVecY[idx] = vy;
 					// if (Math.acos(Math.min(1.0, dot)) < 0.04 / fDistanceToWall) {
 					// bBoundary = true;
 					// }
 					// }
 					// }
+					// if (bBoundary) wallColor = Color.BLACK;
 				}
 			}
 
@@ -284,8 +279,6 @@ public class FPSJFrame extends JPanel implements KeyListener, Runnable {
 			// Wall shading based on distance
 			int shadeIndex = (int) Math.min(fDistanceToWall / fDepth * 10, 9);
 			Color wallColor = wallShades[9 - shadeIndex];
-			// if (bBoundary)
-			// wallColor = Color.BLACK;
 
 			for (int y = 0; y < nScreenHeight; y++) {
 				if (y <= nCeiling) {
